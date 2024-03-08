e4d3bb450b2ffd2a6f9d552ff20230977a3b791c
melonNamedLove <151634498+melonNamedLove@users.noreply.github.com>
2024-03-08 20:50:08 +0900

    demo_build_01
    프로젝트 생성

---
+++ app_dev/jungsukhyeon/app/src/main/java/com/melon/messagebox_demo_01/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.melon.messagebox_demo_01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
